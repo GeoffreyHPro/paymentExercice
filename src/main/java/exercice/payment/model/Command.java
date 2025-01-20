894889530bfcb2752e97f0b1b63c622c161b9739
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-20 17:49:17 +0100

    feat: add command model

---
+++ src/main/java/exercice/payment/model/Command.java
@@ -0,0 +1,71 @@
+package exercice.payment.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "commands")
+public class Command {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idCommand;
+
+    private String productName;
+
+    private String productRef;
+
+    private int quantity;
+
+    private Double price;
+
+    public Command() {
+
+    }
+
+    public Command(String productName, String productRef, int quantity, Double price) {
+        this.productName = productName;
+        this.productRef = productRef;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getIdCommand() {
+        return idCommand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getProductRef() {
+        return productRef;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductRef(String productRef) {
+        this.productRef = productRef;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
